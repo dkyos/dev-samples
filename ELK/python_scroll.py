ec7861954c81b113b0054c431daff0015d2a8690
dk77.yun <dk77.yun@samsung.com>
2017-12-22 11:22:37 +0900

    update
    Signed-off-by: dk77.yun <dk77.yun@samsung.com>

--- ELK/python_scroll.py
+++ ELK/python_scroll.py
@@ -1,12 +1,39 @@
 #!/usr/bin/env python
 #-*- encoding: utf8 -*-
 
+import pandas as pd
 import elasticsearch
 import json
+from pandas import Series, DataFrame
+from pandas.io.json import json_normalize
 
-es_client = elasticsearch.Elasticsearch("localhost:9200")
+total_docs = 0 
+num_docs = 0
 
-docs = es_client.search(index = 'cars',
+es = elasticsearch.Elasticsearch("localhost:9200")
+
+#Process hits here
+def process_hits(hits):
+    global total_docs
+    global num_docs
+
+    num_docs = len(hits)
+    total_docs += num_docs
+    print ("[Total: %d, Num:%d] retrieved" % (total_docs, num_docs))
+
+    df = json_normalize(hits)
+    print (df.shape)
+    print (df.columns)
+
+    '''
+    for item in hits:
+        df = pd.DataFrame(item['_source'], index=[0])
+        print (df.shape)
+        print (df.columns)
+    '''
+
+
+docs = es.search(index = 'cars',
     doc_type = 'cars_doc',
     body = {
         'query': {
@@ -14,15 +41,15 @@ docs = es_client.search(index = 'cars',
         }
     },
     scroll = '1m', 
-    size = 10000) 
+    size = 1000) 
+# Process current batch of hits
+process_hits(docs['hits']['hits'])
 
 scroll_id = docs['_scroll_id']
-
-total_docs = 0 
-num_docs = 1
 while num_docs > 0:
-    docs = es_client.scroll(scroll_id = scroll_id, scroll = '1m')
+    docs = es.scroll(scroll_id = scroll_id, scroll = '1m')
     num_docs = len(docs['hits']['hits'])
-    total_docs += num_docs
-    print ("[Total: %d, Iter:%d] retrieved" % (total_docs, num_docs))
+    if num_docs > 0:
+        # Process current batch of hits
+        process_hits(docs['hits']['hits'])
 
