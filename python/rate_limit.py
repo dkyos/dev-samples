885f2d63b177cab0e52f587bf8abd56dcb17db96
dk77.yun <dk77.yun@samsung.com>
2018-08-10 14:18:53 +0900

    update
    Signed-off-by: dk77.yun <dk77.yun@samsung.com>

--- python/rate_limit.py
+++ python/rate_limit.py
@@ -1,49 +1,38 @@
 #!/usr/bin/env python 
 
-"""global variables for the API URL and rate limiting."""
-
 import functools
-from typing import Callable, Dict, Any, Union, Deque
+from typing import Callable, Deque
 import time
 import threading
 from collections import deque
-import logging
 import inspect
 
-logger = logging.getLogger(__name__)
-
+MAX_CALLS = 10
+PERIOD_SEC = 3
 
 def rate_limited(func: Callable, max_calls: int, period: int) -> Callable:
-    """Limit the number of times a function can be called."""
+    print("Max calls : " + str(max_calls) + " / " + str(period))
     calls: Deque = deque()
 
-    # Add thread safety
     lock = threading.RLock()
 
     @functools.wraps(func)
     def wrapper(*args, **kargs):
-        """Wrap function."""
         with lock:
-            #print(type(calls))
             if len(calls) >= max_calls:
                 until = time.time() + period - (calls[-1] - calls[0])
                 sleeptime = until - time.time()
                 if sleeptime > 0:
-                    logger.info(
-                        f"Rate Limit Reached. (Sleeping for {round(sleeptime)} seconds)"
-                    )
                     print("Rate Limit Reached => sleep with : " + str(sleeptime))
                     time.sleep(sleeptime)
                 while len(calls) > 0:
                     calls.popleft()
             _time = time.time()
             calls.append(_time)
-            print("call append : " + str(len(calls)) + " with : " + str(_time))
-
+            
             # Pop the timestamp list front (ie: the older calls) until the sum goes
             # back below the period. This is our 'sliding period' window.
             while (calls[-1] - calls[0]) >= period:
-                print("call popleft: " + str(len(calls)))
                 calls.popleft()
         
         return func(*args, **kargs)
@@ -53,9 +42,8 @@ def rate_limited(func: Callable, max_calls: int, period: int) -> Callable:
 class Test:
     def __init__(self) -> None:
         for name, fn in inspect.getmembers(self, inspect.ismethod):
-            print(name)
             setattr(
-                self, name, rate_limited(fn, 10, 3)
+                self, name, rate_limited(fn, MAX_CALLS, PERIOD_SEC)
             )
 
     def function_test(self, num):
